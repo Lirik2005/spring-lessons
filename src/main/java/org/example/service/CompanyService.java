de4f7d55e37f8bd5ee1acc2363892efe74c456f0
kgusakov <KGusakov@alfabank.ru>
2024-01-22 17:20:47 +0300

    XML-based_factory_property_injection

---
+++ src/main/java/org/example/service/CompanyService.java
@@ -0,0 +1,10 @@
+package org.example.service;
+
+public class CompanyService {
+
+    private UserService userService;
+
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+}
