a774aa8403f37a9df557d718751c7293adf39788
kgusakov <KGusakov@alfabank.ru>
2024-01-22 16:04:15 +0300

    XML-based_Configuration

--- src/main/java/org/example/ApplicationRunner.java
+++ src/main/java/org/example/ApplicationRunner.java
@@ -1,34 +1,19 @@
 package org.example;
 
 import org.example.database.pool.ConnectionPool;
-import org.example.database.repository.CompanyRepository;
-import org.example.database.repository.UserRepository;
-import org.example.ioc.Container;
-import org.example.service.UserService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
 
-        Container container = new Container();
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
 
         /**
-         * Код приведенный ниже это внедрение зависимостей. Но так писать нельзя, потому что надо создавать огромное количество сущностей
+         * Запись context.getBean(ConnectionPool.class) мы не можем использовать, если у нас есть несколько бинов одного и того же класса.
+         * Поэтому для обращения к конкретному бину используем запись ниже, а именно через id бина
          */
-
-        ConnectionPool pool = new ConnectionPool();
-        UserRepository repositoryUser = new UserRepository(pool);
-        CompanyRepository repositoryCompany = new CompanyRepository(pool);
-        UserService serviceUser = new UserService(repositoryUser, repositoryCompany);
-
-        /**
-         * Код ниже показывает как выглядит инверсия управления. Мы теперь не управляем созданием объекта. Тут, чтобы получить userService
-         * нам не надо создавать connectionPool, userRepository и companyRepository. В итоге мы даже не знаем про зависимости у userService
-         */
-
-        ConnectionPool connectionPool = container.get(ConnectionPool.class);
-        UserRepository userRepository = container.get(UserRepository.class);
-        CompanyRepository companyRepository = container.get(CompanyRepository.class);
-        UserService userService = container.get(UserService.class);
+        ConnectionPool connectionPool = context.getBean("second", ConnectionPool.class);
+        System.out.println(connectionPool);
     }
 }
