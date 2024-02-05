948a3ffe1d7190d9e4c97865542410fee82c0949
kgusakov <KGusakov@alfabank.ru>
2024-02-05 15:45:08 +0300

    Spring Data JPA - Manual Transactions

--- src/test/java/com/spring/integration/annotation/IT.java
+++ src/test/java/com/spring/integration/annotation/IT.java
@@ -3,6 +3,7 @@ package com.spring.integration.annotation;
 import com.spring.integration.TestApplicationRunner;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
@@ -16,6 +17,7 @@ import java.lang.annotation.Target;
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ActiveProfiles("test")
+@Transactional
 @SpringBootTest(classes = TestApplicationRunner.class)
 public @interface IT {
 
--- src/test/java/com/spring/integration/database/repository/CompanyRepositoryTest.java
+++ src/test/java/com/spring/integration/database/repository/CompanyRepositoryTest.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
 import org.springframework.test.annotation.Commit;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.persistence.EntityManager;
 
@@ -16,25 +17,33 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 /**
  * Аннотация @Transactional нужна для управления транзакцией. Если ее убрать, то тест упадет, так как не сможет получить данные о locales.
- * При этом лучше использовать эту аннотацию от Spring.
- *
+ * При этом лучше использовать эту аннотацию от Spring. И мы ее вынесем в аннотацию @IT. Теперь все тесты будут транзакционными.
+ * <p>
  * Аннотация @Commit произведет изменения базы данных и запишет компанию из теста save().
  * По умолчанию, все подобные операции заканчиваются роллбэком.
  */
 @IT
 @RequiredArgsConstructor
-@Transactional
-@Commit
 class CompanyRepositoryTest {
 
     private final EntityManager entityManager;
 
+    /**
+     * TransactionTemplate служит для ручной настройки транзакций и не пользоваться аннотацией @Transactional.
+     * Используется нечасто!!!
+     */
+
+    private final TransactionTemplate transactionTemplate;
+
     @Test
     void findById() {
-        Company company = entityManager.find(Company.class, 1);
+        transactionTemplate.executeWithoutResult(myTransaction -> {
+            Company company = entityManager.find(Company.class, 1);
 
-        assertNotNull(company);
-        assertEquals(2, company.getLocales().size());
+            assertNotNull(company);
+            assertEquals(2, company.getLocales().size());
+        });
+
     }
 
     @Test
