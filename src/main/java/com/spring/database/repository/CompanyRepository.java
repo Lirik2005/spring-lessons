fb7d2de91bcfee4686f0790b1beedf1d394f08c3
kgusakov <KGusakov@alfabank.ru>
2024-02-06 16:58:30 +0300

    Spring Data JPA - Repository_2

--- src/main/java/com/spring/database/repository/CompanyRepository.java
+++ src/main/java/com/spring/database/repository/CompanyRepository.java
@@ -9,6 +9,10 @@ import java.util.Optional;
 /**
  * Интерфейс Repository позволяет отказаться от своих самописных репозиториев. Это автоконфигурация, которая создает repository-слой.
  * В Spring Data JPA не принято использовать конкретные реализации. Поэтому CompanyRepository должен быть интерфейсом.
+ *
+ * У интерфейса Repository есть ряд наследников, один из которых это CrudRepository. Если мы используем его, то нам уже не надо самим
+ * писать такие методы как findById или delete.
+ * Также есть еще JpaRepository, который еще больше расширяет CrudRepository.
  */
 
 public interface CompanyRepository extends Repository<Company, Integer> {
