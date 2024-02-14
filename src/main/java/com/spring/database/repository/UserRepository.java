4be9fa3e501ffafbbc0779586a9cba0b7dd4b667
kgusakov <KGusakov@alfabank.ru>
2024-02-14 15:45:45 +0300

    Spring Data JPA - Special parameters

--- src/main/java/com/spring/database/repository/UserRepository.java
+++ src/main/java/com/spring/database/repository/UserRepository.java
@@ -2,11 +2,15 @@ package com.spring.database.repository;
 
 import com.spring.database.entity.Role;
 import com.spring.database.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
@@ -33,4 +37,26 @@ public interface UserRepository extends JpaRepository<User, Long> {
     @Modifying(clearAutomatically = true)
     @Query("update User u set u.role = :role where u.id in (:ids)")
     int updateRoles(Role role, Long... ids);
+
+    /**
+     * Метод ниже способен вернуть нам первого пользователя с конца
+     */
+
+    Optional<User> findTopByOrderByIdDesc();
+
+    /**
+     * Метод ниже выведет трех пользователей с конца, которые будут отсортированы по указанной дате рождения
+     */
+    List<User> findTop3ByBirthDateBeforeOrderByBirthDateDesc(LocalDate birthDate);
+
+    /**
+     * Метод ниже выведет трех пользователей с конца. Также мы можем передать в метод параметр Sort, который позволит нам делать
+     * динамическую сортировку
+     */
+    List<User> findTop3ByBirthDateBefore(LocalDate birthDate, Sort sort);
+
+    /**
+     * Метод ниже принимает в себя Pageable. Описание смотри в UserRepositoryTest.
+     */
+    List<User> findAllBy(Pageable pageable);
 }
--- src/test/java/com/spring/integration/database/repository/UserRepositoryTest.java
+++ src/test/java/com/spring/integration/database/repository/UserRepositoryTest.java
@@ -6,16 +6,67 @@ import com.spring.database.repository.UserRepository;
 import com.spring.integration.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
+import java.time.LocalDate;
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @IT
 @RequiredArgsConstructor
 class UserRepositoryTest {
 
     private final UserRepository userRepository;
+
+    @Test
+    void checkPageable() {
+        /**
+         * Запись ниже позволяет нам сделать пагинацию с сортировкой по id. Здесь мы получим юзеров начиная со второй страницы (нумерация
+         страниц начинается с нуля и количеством пользователей на странице 2 (плюс сортированных по id)
+         */
+
+        PageRequest pageable = PageRequest.of(1, 2, Sort.by("id"));
+        List<User> result = userRepository.findAllBy(pageable);
+        assertEquals(2, result.size());
+    }
+
+    @Test
+    void checkSort() {
+        /**
+         * Запись ниже позволяет нам динамически осуществлять сортировку в зависимости от класса и отказаться от хардкода.
+         */
+
+        Sort.TypedSort<User> sortBy = Sort.sort(User.class);
+        Sort sort = sortBy.by(User::getFirstname).and(sortBy.by(User::getLastname));
+
+        List<User> sortByName = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), sort);
+        assertEquals(3, sortByName.size());
+    }
+
+    @Test
+    void checkFirstTop() {
+        /**
+         * Сортировку можно делать сложной как в примере ниже
+         */
+
+        List<User> sortByName = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), Sort.by("firstName").and(Sort.by("lastName")));
+        assertEquals(3, sortByName.size());
+
+        List<User> sortById = userRepository.findTop3ByBirthDateBefore(LocalDate.now(), Sort.by("id").descending());
+        assertEquals(3, sortById.size());
+
+        List<User> sortByBirthDate = userRepository.findTop3ByBirthDateBeforeOrderByBirthDateDesc(LocalDate.now());
+        assertEquals(3, sortByBirthDate.size());
+
+        Optional<User> topUser = userRepository.findTopByOrderByIdDesc();
+        assertTrue(topUser.isPresent());
+        topUser.ifPresent(user -> assertEquals(5L, user.getId()));
+    }
 
     @Test
     void checkUpdate() {
