9123f38721d6f7ab5ae6799046cc7d093ab2e1f5
kgusakov <KGusakov@alfabank.ru>
2024-02-26 14:18:41 +0300

    Web Starter - parameters, headers, cookies

--- src/main/java/com/spring/http/controller/GreetingController.java
+++ src/main/java/com/spring/http/controller/GreetingController.java
@@ -1,9 +1,15 @@
 package com.spring.http.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * Аннотация @Controller указывает на то, что наш класс это Spring Controller.
@@ -17,10 +23,23 @@ public class GreetingController {
     /**
      * Аннотация @RequestMapping позволяет нам смапить наш метод с конкретным URL и указать HTTP-метод.
      * Вместо аннотации @RequestMapping можно указать @GetMapping
+     * Аннотация @RequestParam позволяет передавать параметры в url
+     * Аннотация @RequestHeader позволяет передавать нужные хэдеры
+     * Аннотация @CookieValue позволяет передавать нужные куки
+     * По умолчанию аннотации @RequestParam, @RequestHeader и @CookieValue идут как обязательные!!!
+     * <p>
+     * Более того, если название параметра в аннотации совпадает с названием значения, то его можно опустить. Например, можно просто
+     * написать @RequestParam Integer age
+     *
+     * Аннотация @PathVariable нужна, чтобы подставлять переменные в гаш URL
      */
 //    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    @GetMapping("/hello")
-    public ModelAndView hello(ModelAndView modelAndView) {
+    @GetMapping("/hello/{id}")
+    public ModelAndView hello(ModelAndView modelAndView,
+                              @RequestParam("age") Integer age,
+                              @RequestHeader("accept") String accept,
+                              @CookieValue("JSESSIONID") String jsessionId,
+                              @PathVariable("id") Integer id) {
         modelAndView.setViewName("greeting/hello");
 
         return modelAndView;
