bbaaba0938beac49617cf794ce25e6cabd5266af
kgusakov <KGusakov@alfabank.ru>
2024-03-01 17:26:30 +0300

    Thymeleaf Starter - CRUD. View Layer

--- src/main/java/com/spring/http/controller/UserController.java
+++ src/main/java/com/spring/http/controller/UserController.java
@@ -1,17 +1,21 @@
 package com.spring.http.controller;
 
+import com.spring.database.entity.Role;
 import com.spring.dto.UserCreateEditDto;
+import com.spring.service.CompanyService;
 import com.spring.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/users")
@@ -19,6 +23,7 @@ import org.springframework.web.server.ResponseStatusException;
 public class UserController {
 
     private final UserService userService;
+    private final CompanyService companyService;
 
     @GetMapping
     public String findAll(Model model) {
@@ -30,18 +35,40 @@ public class UserController {
     @GetMapping("/{id}")
     public String findById(@PathVariable("id") Long id, Model model) {
         return userService.findById(id)
-                          .map(user -> {
-                              model.addAttribute("user", user);
-                              return "user/user";
-                          })
-                          .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .map(user -> {
+                    model.addAttribute("user", user);
+                    model.addAttribute("roles", Role.values());
+                    model.addAttribute("roles", Role.values());
+                    model.addAttribute("companies", companyService.findAll());
+                    return "user/user";
+                })
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
 
     }
 
+    @GetMapping("/registration")
+    public String registration(Model model, @ModelAttribute("user") UserCreateEditDto user) {
+        model.addAttribute("user", user);
+        model.addAttribute("roles", Role.values());
+        model.addAttribute("companies", companyService.findAll());
+        return "user/registration";
+    }
+
     @PostMapping
 //    @ResponseStatus(HttpStatus.CREATED)
-    public String create(UserCreateEditDto user) {
+    public String create(UserCreateEditDto user, RedirectAttributes redirectAttributes) {
+
+        /**
+         * Логика в if нужна, чтобы в случае ошибки заново не заполнять все поля
+         */
+//        if (true) {
+//            redirectAttributes.addAttribute("username", user.getUsername());
+//            redirectAttributes.addAttribute("firstname", user.getFirstname());
+//            redirectAttributes.addAttribute("lastname", user.getLastname());
+//            redirectAttributes.addFlashAttribute("user", user);
+//            return "redirect:/users/registration";
+//        }
         return "redirect:/users/" + userService.create(user).getId();
     }
 
@@ -49,8 +76,8 @@ public class UserController {
     @PostMapping("/{id}/update")
     public String update(@PathVariable("id") Long id, UserCreateEditDto user) {
         return userService.update(id, user)
-                          .map(it -> "redirect:/users/{id}")
-                          .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+                .map(it -> "redirect:/users/{id}")
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
     //    @DeleteMapping("/{id}")
--- src/main/java/com/spring/service/CompanyService.java
+++ src/main/java/com/spring/service/CompanyService.java
@@ -4,11 +4,13 @@ import com.spring.database.repository.CompanyRepository;
 import com.spring.dto.CompanyReadDto;
 import com.spring.listener.entity.AccessType;
 import com.spring.listener.entity.EntityEvent;
+import com.spring.mapper.CompanyReadMapper;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 /**
@@ -16,18 +18,25 @@ import java.util.Optional;
  */
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional(readOnly = true)
 public class CompanyService {
 
     private final CompanyRepository companyRepository;
-    private final UserService userService;
     private final ApplicationEventPublisher eventPublisher;
+    private final CompanyReadMapper companyReadMapper;
 
     public Optional<CompanyReadDto> findById(Integer id) {
         return companyRepository.findById(id)
-                                .map(entity -> {
-                                    eventPublisher.publishEvent(new EntityEvent(entity, AccessType.READ));
-                                    return new CompanyReadDto(entity.getId(), null);
-                                });
+                .map(entity -> {
+                    eventPublisher.publishEvent(new EntityEvent(entity, AccessType.READ));
+                    return companyReadMapper.map(entity);
+                });
+    }
+
+    public List<CompanyReadDto> findAll() {
+        return companyRepository.findAll()
+                .stream()
+                .map(companyReadMapper::map)
+                .toList();
     }
 }
